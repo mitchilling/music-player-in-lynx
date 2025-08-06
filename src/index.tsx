96aa561ec6844d19fd3b1eb548925b6ec6763349
mitchilling <8478982+mitchilling@users.noreply.github.com>
2025-08-06 12:25:07 +0800

    `step-2`: Build a vertical swipable song list

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import '@lynx-js/react/debug'
 import { root } from '@lynx-js/react'
 
-import { App } from './App.jsx'
+// import { App } from './App.jsx'
+import { SongList } from './components/SongList.jsx'
 
-root.render(<App />)
+root.render(<SongList />)
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
