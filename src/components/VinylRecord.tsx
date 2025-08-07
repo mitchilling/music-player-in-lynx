34be81e329c4b0a9c98ca1a8358d5cfe29cbcd0a
mitchilling <8478982+mitchilling@users.noreply.github.com>
2025-08-07 14:47:55 +0800

    `step-8`: Enable vertical swiping and update song titles

--- src/components/VinylRecord.tsx
+++ src/components/VinylRecord.tsx
@@ -1,7 +1,7 @@
 import type { Song } from '../model/Song.jsx';
 
 export interface VinylRecordProps {
-  song: Song | null;
+  title: string | null;
   isPlaying: boolean;
 }
 
@@ -11,7 +11,7 @@ export const VinylRecord = (props: VinylRecordProps) => {
       style={{ width: "100%", height: "75vh", display: "flex", justifyContent: "center", alignItems: "center" }}
     >
       <text style={{ fontSize: "16px", paddingLeft: "6px", paddingTop: "6px" }}>
-        {`${props.song ? props.song.title : "No song playing"}`}
+        {`${props.title ? props.title : "Empty Playlist"}`}
       </text>
     </view>
   );
